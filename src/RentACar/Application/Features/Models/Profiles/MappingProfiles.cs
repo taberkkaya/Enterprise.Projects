3bfca72ce53c5ca7796f41d79f220d00132ef897
akaya <taberkkaya@gmail.com>
2024-10-14 02:48:00 +0300

    migration ops done

---
+++ src/RentACar/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Models.Queries.GetList;
+using AutoMapper;
+using Core.Application.Responses;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Features.Models.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Model, GetListModelListItemDto>().ReverseMap();
+        CreateMap<IPaginate<Model>, GetListResponse<GetListModelListItemDto>>().ReverseMap();
+    }
+}
--- src/RentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/RentACar/Persistence/PersistenceServiceRegistration.cs
@@ -11,8 +11,11 @@ public static class PersistenceServiceRegistration
 {
     public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitecture"));
+        //services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitecture"));
+        services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("RentACar")));
+
         services.AddScoped<IBrandRepository, BrandRepository>();
+        services.AddScoped<IModelRepository, ModelRepository>();
 
         return services;
     }
