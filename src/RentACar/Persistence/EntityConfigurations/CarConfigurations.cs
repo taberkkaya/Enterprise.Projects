009ae827d611905e4ca0570e30c471e7ad981234
akaya <taberkkaya@gmail.com>
2024-10-14 01:48:36 +0300

    Brand,Car,Model,Fuel,Transmission entities and repos created

--- src/RentACar/Persistence/EntityConfigurations/CarConfigurations.cs
+++ src/RentACar/Persistence/EntityConfigurations/CarConfigurations.cs
@@ -4,20 +4,24 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Persistence.EntityConfigurations;
 
-public class BrandConfigurations : IEntityTypeConfiguration<Brand>
+public class CarConfigurations : IEntityTypeConfiguration<Car>
 {
-    public void Configure(EntityTypeBuilder<Brand> builder)
+    public void Configure(EntityTypeBuilder<Car> builder)
     {
-        builder.ToTable("Brands").HasKey(b => b.Id);
+        builder.ToTable("Cars").HasKey(b => b.Id);
 
         builder.Property(b => b.Id).HasColumnName("Id").IsRequired();
-        builder.Property(b => b.Name).HasColumnName("Name").IsRequired();
+
+        builder.Property(b => b.ModelId).HasColumnName("ModelId").IsRequired();
+        builder.Property(b => b.Kilometer).HasColumnName("Kilometer").IsRequired();
+        builder.Property(b => b.CarState).HasColumnName("CarState").IsRequired();
+        builder.Property(b => b.ModelYear).HasColumnName("ModelYear").IsRequired();
+
         builder.Property(b => b.CreatedDate).HasColumnName("CreatedDate").IsRequired();
         builder.Property(b => b.UpdatedDate).HasColumnName("UpdatedDate");
         builder.Property(b => b.DeletedDate).HasColumnName("DeletedDate");
 
-        builder.HasIndex(indexExpression: b=> b.Name, name:"UK_Brands_Name").IsUnique();
-        builder.HasMany(b => b.Models);
+        builder.HasOne(b => b.Model);
 
         builder.HasQueryFilter(b => !b.DeletedDate.HasValue);
     }
--- src/RentACar/Persistence/EntityConfigurations/FuelConfigurations.cs
+++ src/RentACar/Persistence/EntityConfigurations/FuelConfigurations.cs
@@ -4,11 +4,11 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Persistence.EntityConfigurations;
 
-public class BrandConfigurations : IEntityTypeConfiguration<Brand>
+public class FuelConfigurations : IEntityTypeConfiguration<Fuel>
 {
-    public void Configure(EntityTypeBuilder<Brand> builder)
+    public void Configure(EntityTypeBuilder<Fuel> builder)
     {
-        builder.ToTable("Brands").HasKey(b => b.Id);
+        builder.ToTable("Fuels").HasKey(b => b.Id);
 
         builder.Property(b => b.Id).HasColumnName("Id").IsRequired();
         builder.Property(b => b.Name).HasColumnName("Name").IsRequired();
@@ -16,7 +16,8 @@ public class BrandConfigurations : IEntityTypeConfiguration<Brand>
         builder.Property(b => b.UpdatedDate).HasColumnName("UpdatedDate");
         builder.Property(b => b.DeletedDate).HasColumnName("DeletedDate");
 
-        builder.HasIndex(indexExpression: b=> b.Name, name:"UK_Brands_Name").IsUnique();
+        builder.HasIndex(indexExpression: b => b.Name, name: "UK_Fuels_Name").IsUnique();
+
         builder.HasMany(b => b.Models);
 
         builder.HasQueryFilter(b => !b.DeletedDate.HasValue);
