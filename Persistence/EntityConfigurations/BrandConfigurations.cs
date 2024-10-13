d5ac3730ba96d83466c44555c22bbe083b57e999
akaya <taberkkaya@gmail.com>
2024-10-13 03:47:25 +0300

    Add project files.

---
+++ Persistence/EntityConfigurations/BrandConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityConfigurations;
+
+public class BrandConfigurations : IEntityTypeConfiguration<Brand>
+{
+    public void Configure(EntityTypeBuilder<Brand> builder)
+    {
+        builder.ToTable("Brands").HasKey(b => b.Id);
+
+        builder.Property(b => b.Id).HasColumnName("Id").IsRequired();
+        builder.Property(b => b.Id).HasColumnName("Name").IsRequired();
+        builder.Property(b => b.Id).HasColumnName("CreatedDate").IsRequired();
+        builder.Property(b => b.Id).HasColumnName("UpdatedDate");
+        builder.Property(b => b.Id).HasColumnName("DeletetedDate");
+
+        builder.HasQueryFilter(b => !b.DeletedDate.HasValue);
+    }
+}
---
+++ WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Queries.GetList;
+using Core.Application.Requests;
+using Core.Application.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BrandsController : BaseController
+{
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+    {
+        CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+        return Ok(response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
+        GetListResponse<GetListBrandListItemDto> result = await Mediator.Send(getListBrandQuery);
+        return Ok(result);
+    }
+}
