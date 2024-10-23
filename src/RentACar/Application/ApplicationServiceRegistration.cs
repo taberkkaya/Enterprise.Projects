bb4119a659475fd045939ef69baa85bf8eab15b7
akaya <taberkkaya@gmail.com>
2024-10-23 23:38:22 +0300

    Converted log implementation to mssql log

--- src/RentACar/Application/ApplicationServiceRegistration.cs
+++ src/RentACar/Application/ApplicationServiceRegistration.cs
@@ -31,7 +31,7 @@ public static class ApplicationServiceRegistration
             configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
-        services.AddSingleton<LoggerServiceBase, FileLogger>();
+        services.AddSingleton<LoggerServiceBase, MsSqlLogger>();
 
         return services;
     }
